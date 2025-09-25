5b64912aef429aa25ddc3ec8b1b6f1666a4046d6
Dan Cristoloveanu <dcristo@microsoft.com>
2025-09-25 11:16:16 -0700

    [MrBot] Migrate unit tests to ENABLE_TEST_FILES_PRECOMPILED_HEADERS (#199)
    * Update c-testrunnerswitcher to latest master and migrate unit tests to use ENABLE_TEST_FILES_PRECOMPILED_HEADERS
    
    - Update c-testrunnerswitcher submodule to commit b4f7beb which adds ENABLE_TEST_FILES_PRECOMPILED_HEADERS support
    - Add migrate_to_pch.ps1 script for migrating unit tests from MOCK_PRECOMPILE_HEADERS to ENABLE_TEST_FILES_PRECOMPILED_HEADERS
    - Migrate com_wrapper_ut to use precompiled headers with dedicated PCH header file
    - Generated com_wrapper_ut_pch.h contains all includes needed for precompilation
    - Updated CMakeLists.txt to use ENABLE_TEST_FILES_PRECOMPILED_HEADERS option
    
    * Remove migration script
    
    The migrate_to_pch.ps1 script was used for the one-time migration and is no longer needed in the repository.

--- tests/com_wrapper_ut/com_wrapper_ut.c
+++ tests/com_wrapper_ut/com_wrapper_ut.c
@@ -1,36 +1,8 @@
-// Copyright (c) Microsoft. All rights reserved.
+﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#include <stdlib.h>
-#include <stddef.h>
+#include "com_wrapper_ut_pch.h"
 
-#include "macro_utils/macro_utils.h"
-
-#include "testrunnerswitcher.h"
-#include "umock_c/umock_c.h"
-#include "umock_c/umocktypes.h"
-#include "umock_c/umocktypes_charptr.h"
-#include "c_logging/logger.h"
-
-#include "c_pal/interlocked.h" /*included for mocking reasons - it will prohibit creation of mocks belonging to interlocked.h - at the moment verified through int tests - this is porting legacy code, temporary solution*/
-
-#define ENABLE_MOCKS
-#include "c_pal/gballoc_hl.h"
-#include "c_pal/gballoc_hl_redirect.h"
-#include "test_object.h"
-
-#undef ENABLE_MOCKS
-
-#include "real_gballoc_hl.h"
-
-#include "com_wrapper/com_wrapper.h"
-
-#include "Unknwnbase.h"
-#include "test_interface.h"
-#include "test_object_2.h"
-#include "real_test_object.h"
-#include "test_object_wrapper.h"
-#include "test_object_2_wrapper.h"
 
 static REFIID iid_IUnknown = &IID_IUnknown;
 static REFIID iid_ITestInterface = &IID_ITestInterface;
