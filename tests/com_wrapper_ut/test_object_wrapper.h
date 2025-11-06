bdd0c2cc3b3a6c851a74063a2ba13f6a0cbb9041
Dan Cristoloveanu <dcristo@microsoft.com>
2025-11-05 19:14:12 -0800

    Fix spec for DECLARE_COM_WRAPPER_OBJECT

--- tests/com_wrapper_ut/test_object_wrapper.h
+++ tests/com_wrapper_ut/test_object_wrapper.h
@@ -23,7 +23,7 @@
         COM_WRAPPER_FUNCTION_WRAPPER(long, test_object_TestMethodWithReturnWith2Args, int, a, const char*, b) \
     )
 
-/* Tests_SRS_COM_WRAPPER_01_027: [ DEFINE_COM_WRAPPER_OBJECT shall generate constructor prototypes for the COM object for each implemented interface. ]*/
+/* Tests_SRS_COM_WRAPPER_01_027: [ DECLARE_COM_WRAPPER_OBJECT shall generate constructor prototypes for the COM object for each implemented interface. ]*/
 DECLARE_COM_WRAPPER_OBJECT(TEST_OBJECT_HANDLE, TEST_OBJECT_HANDLE_INTERFACES);
 
 #endif /* TEST_OBJECT_WRAPPER_H */
